c76c3c35af47b535f859c830f142670e4ef62dd0
dzachara <dzachara@ZACHARA-D>
2021-12-16 13:30:40 +0100

    Add project files.

---
+++ L2_7_Zad_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L2_7_Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This app will check that entered number is even or odd.");
+            Console.Write("Enter an integer number: ");
+            Int32.TryParse(Console.ReadLine(), out int numberInt);
+            if(numberInt % 2 == 0)
+            {
+                Console.WriteLine($"Entered number {numberInt} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"Entered number {numberInt} is odd.");
+            }
+        }
+    }
+}
